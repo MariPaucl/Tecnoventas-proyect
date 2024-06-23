a90a855e2a4bc2fae75080ddc05985bc970e7202
lisbeth <lisbeth.pieros@misena.com>
2024-06-23 10:34:48 -0500

    Se agrega la funcionalidad de aumentar productos en el carrito

--- carrito/AccionCarta.php
+++ carrito/AccionCarta.php
@@ -1,15 +1,15 @@
 <?php
 date_default_timezone_set("America/Bogota");
-// Iniciamos la clase de la carta
 include 'La-carta.php';
 $cart = new Cart;
 
-// include database configuration file
+
 include 'conexion.php';
+
+
 if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
     if($_REQUEST['action'] == 'addToCart' && !empty($_REQUEST['codProd'])){
         $productID = $_REQUEST['codProd'];
-        // get product details
         $query = $db->query("SELECT * FROM productos WHERE codProd = ".$productID);
         $row = $query->fetch_assoc();
         $itemData = array(
@@ -19,96 +19,94 @@ if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
             'qty' => 1
         );
 
+
         $insertItem = $cart->insert($itemData);
-        $redirectLoc = $insertItem?'VerCarta.php':'../cell.php';
+        $redirectLoc = $insertItem ? 'VerCarta.php' : '../cell.php';
         header("Location: ".$redirectLoc);
-    }elseif($_REQUEST['action'] == 'updateCartItem' && !empty($_REQUEST['codProd'])){
+    } elseif($_REQUEST['action'] == 'updateCartItem' && !empty($_REQUEST['rowid'])){
         $itemData = array(
-            'rowid' => $_REQUEST['codProd'],
+            'rowid' => $_REQUEST['rowid'],
             'qty' => $_REQUEST['qty']
         );
         $updateItem = $cart->update($itemData);
-        echo $updateItem?'ok':'err';die;
-    }elseif($_REQUEST['action'] == 'removeCartItem' && !empty($_REQUEST['codProd'])){
+        echo $updateItem ? 'ok' : 'err';
+        die;
+    } elseif($_REQUEST['action'] == 'removeCartItem' && !empty($_REQUEST['codProd'])){
         $deleteItem = $cart->remove($_REQUEST['codProd']);
         header("Location: VerCarta.php");
-    }elseif($_REQUEST['action'] == 'placeOrder' && $cart->total_items() > 0 && !empty($_SESSION['sessCustomerID'])){
-        // Obtén el valor de la dirección del formulario
-    $direccion = isset($_POST['direccion']) ? $_POST['direccion'] : '';
+    } elseif($_REQUEST['action'] == 'placeOrder' && $cart->total_items() > 0 && !empty($_SESSION['sessCustomerID'])){
+        $direccion = isset($_POST['direccion']) ? $_POST['direccion'] : '';
 
-    $cartItems = $cart->contents();
 
-    $haySuficienteStock = true;
+        $cartItems = $cart->contents();
+        $haySuficienteStock = true;
 
-    foreach ($cartItems as $item) {
-        $codProd = $item['codProd'];
-        $qty = $item['qty'];
 
-        // Obtener el stock actual del producto
-        $result = $db->query("SELECT stockProd, nomProd FROM productos WHERE codProd = '$codProd'");
-        $row = $result->fetch_assoc();
-        $stockActual = $row['stockProd'];
-        $nomProd = $row['nomProd'];
+        foreach ($cartItems as $item) {
+            $codProd = $item['codProd'];
+            $qty = $item['qty'];
+            $result = $db->query("SELECT stockProd, nomProd FROM productos WHERE codProd = '$codProd'");
+            $row = $result->fetch_assoc();
+            $stockActual = $row['stockProd'];
+            $nomProd = $row['nomProd'];
 
-        // Verificar si hay suficiente stock
-        if ($qty > $stockActual) {
-            $haySuficienteStock = false;
-            echo "<script>alert('Error: No hay suficiente stock para el producto $nomProd. Stock: $stockActual');</script>";
-            echo "<script>window.location.href = 'VerCarta';</script>";
-            exit; // Asegúrate de salir del script después de la redirección
-        }
-    }
 
-    // Si hay suficiente stock, proceder con la inserción del pedido
-    if ($haySuficienteStock) {
-
-        $iva = $cart->total() * 0.20;
-        $total = $cart->total() + $iva;
-
-    // Inserta el pedido en la base de datos con la dirección
-    $insertOrder = $db->query("INSERT INTO pedidos (idCliente, fechaPedido, horaPedido, dirPedido, totalPedido) VALUES ('".$_SESSION['sessCustomerID']."', '".date("Y-m-d")."', '".date("H:i:s")."', '".$direccion."', '".$total."')");
-
-        if($insertOrder){
-            $orderID = $db->insert_id;
-            $sql = '';
-            // get cart items
-            $cartItems = $cart->contents();
-            foreach($cartItems as $item){
-
-                $codProd = $item['codProd'];
-                $qty = $item['qty'];
-
-                // Update stock in the productos table
-                $updateStockQuery = "UPDATE productos SET stockProd = stockProd - $qty WHERE codProd = '$codProd';";
-                $db->query($updateStockQuery);
-
-                // Comprobar si el stock es cero y actualizar el estado
-                $result = $db->query("SELECT stockProd FROM productos WHERE codProd = '$codProd'");
-                $row = $result->fetch_assoc();
-                if($row['stockProd'] == 0){
-                    $updateEstProdQuery = "UPDATE productos SET estProd = 'Agotado' WHERE codProd = '$codProd';";
-                    $db->query($updateEstProdQuery);
-
-                }
-                
-                $sql .= "INSERT INTO detallepedidos (codPedido, codProd, cantidadProd) VALUES ('".$orderID."', '".$item['codProd']."', '".$item['qty']."');";
+            if ($qty > $stockActual) {
+                $haySuficienteStock = false;
+                echo "<script>alert('Error: No hay suficiente stock para el producto $nomProd. Stock: $stockActual');</script>";
+                echo "<script>window.location.href = 'VerCarta';</script>";
+                exit;
             }
-            // insert order items into database
-            $insertOrderItems = $db->multi_query($sql);
+        }
 
-            if($insertOrderItems){
-                $cart->destroy();
-                header("Location: OrdenExito.php?id=$orderID");
-            }else{
+
+        if ($haySuficienteStock) {
+            $iva = $cart->total() * 0.20;
+            $total = $cart->total() + $iva;
+            $insertOrder = $db->query("INSERT INTO pedidos (idCliente, fechaPedido, horaPedido, dirPedido, totalPedido) VALUES ('".$_SESSION['sessCustomerID']."', '".date("Y-m-d")."', '".date("H:i:s")."', '".$direccion."', '".$total."')");
+
+
+            if($insertOrder){
+                $orderID = $db->insert_id;
+                $sql = '';
+                foreach($cartItems as $item){
+                    $codProd = $item['codProd'];
+                    $qty = $item['qty'];
+
+
+                    $updateStockQuery = "UPDATE productos SET stockProd = stockProd - $qty WHERE codProd = '$codProd';";
+                    $db->query($updateStockQuery);
+
+
+                    $result = $db->query("SELECT stockProd FROM productos WHERE codProd = '$codProd'");
+                    $row = $result->fetch_assoc();
+                    if($row['stockProd'] == 0){
+                        $updateEstProdQuery = "UPDATE productos SET estProd = 'Agotado' WHERE codProd = '$codProd';";
+                        $db->query($updateEstProdQuery);
+                    }
+
+
+                    $sql .= "INSERT INTO detallepedidos (codPedido, codProd, cantidadProd) VALUES ('".$orderID."', '".$item['codProd']."', '".$item['qty']."');";
+                }
+                $insertOrderItems = $db->multi_query($sql);
+
+
+                if($insertOrderItems){
+                    $cart->destroy();
+                    header("Location: OrdenExito.php?id=$orderID");
+                } else {
+                    header("Location: Pagos.php");
+                }
+            } else {
                 header("Location: Pagos.php");
             }
-        }else{
-            header("Location: Pagos.php");
+        } else {
+            header("Location: ../cell.php");
         }
-    }else{
+    } else {
         header("Location: ../cell.php");
     }
-}else{
-    header("Location: ../cell.php");
 }
-}
+?>
+
+
--- carrito/VerCarta.php
+++ carrito/VerCarta.php
@@ -6,6 +6,7 @@ $cart = new Cart;
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <title>Carrito de Compras</title>
     <meta charset="utf-8">
@@ -26,14 +27,17 @@ $cart = new Cart;
             border: none;
         }
 
+
         h1{
             color: rgb(46, 1, 82);
             text-transform: uppercase;
         }
 
+
         .container {
             padding: 20px;
         }
+
 
         input[type="number"] {
             max-width: 80%;
@@ -41,28 +45,25 @@ $cart = new Cart;
             border: none;
         }
     </style>
-    <script>
-        function updateCartItem(obj, id) {
-            console.log('Updating cart item:', id, 'New quantity:', obj.value);
-            $.get("AccionCarta.php", {
-                action: 'updateCartItem',
-                id: id,
-                qty: obj.value
-            }, function(data) {
-                if (data.includes('ok')) {
-                    console.log(data)
-                    location.reload();
-                } else {
-                    alert('No se ha podido actualizar el carrito, intente de nuevo!');
-                }
-            });
-        }
+     <script>
+    function updateCartItem(rowid, qty){
+        $.get("AccionCarta.php", {action: "updateCartItem", rowid: rowid, qty: qty}, function(data){
+            if(data == 'ok'){
+                location.reload();
+            }else{
+                alert('Error al actualizar el carrito, por favor intente de nuevo.');
+            }
+        });
+    }
     </script>
 </head>
-</head>
+
+
+
 
 <body>
     <div class="container">
+
 
                 <h1>Carrito de compras</h1>
                 <table class="table">
@@ -85,7 +86,7 @@ $cart = new Cart;
                                 <tr>
                                     <td><?php echo $item["nomProd"]; ?></td>
                                     <td><?php echo '$' . $item["precio"]; ?></td>
-                                    <td><input type="number" readonly class="form-control text-center" value="<?php echo $item['qty']; ?>" onchange="updateCartItem(this, '<?php echo $item['rowid']; ?>')"></td>
+                                    <td><input type="number" value="<?php echo $item['qty']; ?>" onchange="updateCartItem('<?php echo $item['rowid']; ?>', this.value)"></td>
                                     <td><?php echo '$' . $item["subtotal"]; ?></td>
                                     <td>
                                         <a href="AccionCarta.php?action=removeCartItem&codProd=<?php echo $item["rowid"]; ?>" class="btn btn-danger" onclick="return confirm('Confirma eliminar?')"><i class="fa-solid fa-trash"></i></a>
@@ -116,3 +117,4 @@ $cart = new Cart;
     </div>
 </body>
 </html>
+
